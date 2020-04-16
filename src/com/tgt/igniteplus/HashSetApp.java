18980fc2a69af856dde7469376e9606355509e83
unknown <gmcharitha@gmail.com>
2020-04-16 10:37:42 +0530

    uploaded

---
+++ src/com/tgt/igniteplus/HashSetApp.java
@@ -0,0 +1,20 @@
+package com.tgt.igniteplus;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetApp
+{
+    public static void main(String[] args)
+    {
+        Set<String> skill = new HashSet<>();
+        skill.add("Java");
+        skill.add("SQL");
+        skill.add("NoSQL");
+        skill.add("Java");
+        for (String str: skill)
+        {
+            System.out.println(str);
+        }
+    }
+}
